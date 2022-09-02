21b1efc39f38363f2036f9325aaf66653adbe6da
bryn-ellison <bryn.ellison@outlook.com>
2022-09-02 17:05:15 +0100

    button added which prompts gridsize input and resets previous grid

--- javascript.js
+++ javascript.js
@@ -1,12 +1,31 @@
+const body = document.querySelector("body");
 const container = document.querySelector("#container");
+let gridsize = 0;
+const button = document.createElement("button");
+button.innerText = "Create a new grid";
+button.addEventListener("click", () => {
+  gridWidth = prompt("What size grid do you want? (enter width, maximum 100)");
+  reset(gridWidth);
+});
+body.appendChild(button);
 
-function createGrid() {
-  for (let i = 1; i < 257; i++) {
-    const div = document.createElement("div");
-    div.classList.add("grid-square");
-    container.appendChild(div);
-    console.log(div);
+function reset(gridWidth) {
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
   }
+  createGrid(gridWidth);
 }
 
-createGrid();
+function createGrid(gridWidth) {
+  const gridSize = gridWidth * gridWidth;
+
+  for (let i = 1; i < gridSize; i++) {
+    const div = document.createElement("div");
+    document.documentElement.style.setProperty("--rowNum", gridWidth);
+    div.classList.add("grid-square");
+    div.addEventListener("mouseenter", (e) => {
+      div.classList.add("hover");
+    });
+    container.appendChild(div);
+  }
+}
